6107431eb89b8eb55bc3c9bf49e747e50fd3cfea
Andrea Mafessoni <andrea.mafessoni@cefriel.com>
2021-01-04 19:40:05 +0100

    FattureTableComponent added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FattureTableComponent } from './fatture-table/fatture-table.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
@@ -12,7 +13,8 @@ import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FattureTableComponent
   ],
   imports: [
     BrowserModule,
