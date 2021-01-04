046b5228cd3be8b087685b308f420027abd2c376
Andrea Mafessoni <andrea.mafessoni@cefriel.com>
2021-01-04 18:56:26 +0100

    Test DB

--- server.js
+++ server.js
@@ -1,14 +1,59 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = process.env.MONGODB_URI;
 
-console.log(process.env.MONGODB_URI),
+MongoClient.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(client =>{
+  console.log("Succesfully connected to DB!")
+  app.use(express.json()); // for parsing application/json
 
-app.use(express.static(__dirname + '/dist/fatture-manager'));
+  const fattureCollection = client.db("codigital-DB").collection("fatture");
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/fatture-manager/index.html'));
+  app.listen(process.env.PORT || 8080, function() {
+    console.log('listening on 3000');
+  })
+
+  fattureCollection.find().toArray()
+  .then(fatture => {
+    console.log("Succesfully connected to collection ''fatture''!")
+    app.use(express.static(__dirname + '/dist/fatture-manager'));
+
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname+'/dist/fatture-manager/index.html'));
+    });
+
+    //GET
+    app.get('/fatture', (req, res) => {
+      res.send(fatture);
+    });
+
+    //POST
+    app.post('/fatture', (req, res) => {
+      console.log(req)
+
+      if (req.body.nome && req.body.prezzo) {
+        prodottiCollection.insertOne(req.body)
+        .then(result => {
+          console.log(result)
+        })
+        .catch(error => console.error(error))
+      }
+
+      res.send(req.body)
+    });
+
+    //PUT
+
+    //DELETE
+
+  })
+
+
+
+})
+.catch(err=>{
+  console.log(`db error ${err.message}`);
+  process.exit(-1)
 });
-
-app.listen(process.env.PORT || 8080);
